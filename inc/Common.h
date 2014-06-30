f6d4e215731e33844bd22e9a709a39f70246abb1
Voldymyr <vova.bendiuga@gmail.com>
2014-06-30 12:31:52 +0300

    added gameWindowModel to old window data

--- inc/Common.h
+++ inc/Common.h
@@ -8,7 +8,19 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
-enum ElementType {Yellow, Green};
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
+/* Types of elements on the game board */
+enum ElementType {Yellow, Green, Blue, Purple, Red, None};
+
+/* Common structure representing position of objects on the 2D game board */
+struct Position
+{
+	float x;
+	float y;
+};
 
 #endif /* COMMON_H_ */
---
+++ inc/GameWindowModel.h
@@ -0,0 +1,18 @@
+/*
+ * GameWindowModel.h
+ *
+ *  Created on: Jun 30, 2014
+ *      Author: khrystynka
+ */
+
+#ifndef GAMEWINDOWMODEL_H_
+#define GAMEWINDOWMODEL_H_
+
+#include "../inc/GraphicsEngine.h"
+
+namespace Model
+{
+
+}
+
+#endif /* GAMEWINDOWMODEL_H_ */
