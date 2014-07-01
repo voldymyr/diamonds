b4d1425f820447c16fad7ac4b5b45cd75f9c63bf
Voldymyr <vova.bendiuga@gmail.com>
2014-07-01 12:17:42 +0300

    updated game window model

--- inc/GameWindowModel.h
+++ inc/GameWindowModel.h
@@ -1,6 +1,9 @@
 /*
  * GameWindowModel.h
  *
+ * Currently this class is dependent on the SDL library
+ * It should be abstracted from SDL by GraphicsEngine
+ *
  *  Created on: Jun 30, 2014
  *      Author: khrystynka
  */
@@ -20,9 +23,27 @@ namespace Model
 		~GameWindowModel(void);
 		void SetMainWindow(SDL_Surface **window);
 		SDL_Surface* GetMainWIndow(void);
+		void SetBackGroundIMG(SDL_Surface **img);
+		SDL_Surface* GetBackGroundIMG(void);
+		void SetBackGroundRect(SDL_Rect *rect);
+		SDL_Rect GetBackGroundRect(void);
+		void SetTextFont(TTF_Font **font);
+		TTF_Font* GetTextFont(void);
+		void SetTextColor(SDL_Color *color);
+		SDL_Color GetTextColor(void);
+		void SetGameOverStr(SDL_Surface ** surfaceStr);
+		SDL_Surface* GetGameOverStr(void);
+		void SetGameOverRect(SDL_Rect *rect);
+		SDL_Rect GetGameOverRect(void);
 
 	protected:
-		SDL_Surface *mainWindow;
+		SDL_Surface* mainWindow;
+		SDL_Surface* backGroundIMG;
+		SDL_Rect backGroundRect;
+		TTF_Font* textFont;
+		SDL_Color textColor;
+		SDL_Surface* gameOverStr;
+		SDL_Rect gameOverRect;
 	};
 }
 
--- src/GameWindowModel.cpp
+++ src/GameWindowModel.cpp
@@ -10,6 +10,12 @@
 Model::GameWindowModel::GameWindowModel()
 {
 	mainWindow = NULL;
+	backGroundIMG = NULL;
+	backGroundRect = {0, 0, 0, 0};
+	textFont = NULL;
+	textColor = {0, 0, 0};
+	gameOverStr = NULL;
+	gameOverRect = {0, 0, 0, 0};
 }
 
 Model::GameWindowModel::~GameWindowModel()
@@ -24,4 +30,64 @@ void Model::GameWindowModel::SetMainWindow(SDL_Surface **window)
 SDL_Surface* Model::GameWindowModel::GetMainWIndow()
 {
 	return mainWindow;
+}
+
+void Model::GameWindowModel::SetBackGroundIMG(SDL_Surface **img)
+{
+	backGroundIMG = *img;
+}
+
+SDL_Surface* Model::GameWindowModel::GetBackGroundIMG()
+{
+	return backGroundIMG;
+}
+
+void Model::GameWindowModel::SetBackGroundRect(SDL_Rect *rect)
+{
+	backGroundRect = *rect;
+}
+
+SDL_Rect Model::GameWindowModel::GetBackGroundRect()
+{
+	return backGroundRect;
+}
+
+void Model::GameWindowModel::SetTextFont(TTF_Font **font)
+{
+	textFont = *font;
+}
+
+TTF_Font* Model::GameWindowModel::GetTextFont()
+{
+	return textFont;
+}
+
+void Model::GameWindowModel::SetTextColor(SDL_Color *color)
+{
+	textColor = color;
+}
+
+SDL_Color Model::GameWindowModel::GetTextColor()
+{
+	return textColor;
+}
+
+void Model::GameWindowModel::SetGameOverStr(SDL_Surface **surfaceStr)
+{
+	gameOverStr = *surfaceStr;
+}
+
+SDL_Surface* Model::GameWindowModel::GetGameOverStr()
+{
+	return gameOverStr;
+}
+
+void Model::GameWindowModel::SetGameOverRect(SDL_Rect *rect)
+{
+	gameOverRect = *rect;
+}
+
+SDL_Rect Model::GameWindowModel::GetGameOverRect()
+{
+	return gameOverRect;
 }
