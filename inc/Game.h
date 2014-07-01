c77297c5ac48d3aca6ec0b44cfa1774076bc050e
Voldymyr <vova.bendiuga@gmail.com>
2014-07-01 16:26:35 +0300

    specific game files added

---
+++ inc/Game.h
@@ -0,0 +1,21 @@
+/*
+ * Game.h
+ *
+ *	Game is a class that represents this game, the Diamonds.
+ *	This class contains game engine and uses its functionality to
+ *	construct the game.
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: khrystynka
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game
+{
+
+};
+
+
+#endif /* GAME_H_ */
---
+++ src/Game.cpp
@@ -0,0 +1,12 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: khrystynka
+ */
+
+#include "../inc/Game.h"
+
+
+
+
