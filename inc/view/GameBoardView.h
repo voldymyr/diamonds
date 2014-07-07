2896dd5ae7cf2e95806368b741d4dd220fc299bc
Voldymyr <vova.bendiuga@gmail.com>
2014-07-07 18:20:41 +0300

    GameBoardView and Model added

---
+++ inc/view/GameBoardView.h
@@ -0,0 +1,22 @@
+/*
+ * GameBoardView.h
+ *
+ *  Created on: Jul 7, 2014
+ *      Author: khrystynka
+ */
+
+#ifndef GAMEBOARDVIEW_H_
+#define GAMEBOARDVIEW_H_
+
+namespace View
+{
+	class GameBoardView
+	{
+	public:
+		GameBoardView();
+		~GameBoardView();
+	};
+}
+
+
+#endif /* GAMEBOARDVIEW_H_ */
---
+++ src/view/GameBoardView.cpp
@@ -0,0 +1,22 @@
+/*
+ * GameBoardView.cpp
+ *
+ *  Created on: Jul 7, 2014
+ *      Author: khrystynka
+ */
+
+#include "../../inc/view/GameBoardView.h"
+
+
+View::GameBoardView::GameBoardView()
+{
+
+}
+
+View::GameBoardView::~GameBoardView()
+{
+
+}
+
+
+
