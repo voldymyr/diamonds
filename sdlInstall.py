f130934a5601d45652b9f6a786326c29f940a725
aliks <vova.bendiuga@gmail.com>
2014-10-17 17:28:31 +0200

    Python script added to automatically install SDL library and its components

---
+++ sdlInstall.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Python script for downloading and installing SDl1.2 Development ibrary on Linux
+# @author Alisk
+
+import os
+
+os.system("date")
+print "\n********** Preparing to install SDL1.2 Development Library **********\n"
+print "\t BE READY TO ENTER YOUR PASSWORD\n"
+
+os.system("sudo apt-get install -y libsdl1.2-dev")
+os.system("sudo apt-get install -y libsdl-image1.2-dev")
+os.system("sudo apt-get install -y libsdl-mixer1.2-dev")
+os.system("sudo apt-get install -y libsdl-ttf2.0-dev")
+
+print "\n\t\t********** DONE **********\n"
